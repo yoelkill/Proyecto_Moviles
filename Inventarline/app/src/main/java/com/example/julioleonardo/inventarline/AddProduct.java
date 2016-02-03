99ad6b1508f0e73878337ff9761ca0cbbc217e56
Julio César Leonardo Paredes <julio.cesar.leonardo.paredes@gmail.com>
2016-02-03 00:35:48 -0500

    Inventarline
    Proyecto Móvil

---
+++ Inventarline/app/src/main/java/com/example/julioleonardo/inventarline/AddProduct.java
@@ -0,0 +1,14 @@
+package com.example.julioleonardo.inventarline;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Julio Leonardo on 2/02/2016.
+ */
+public class AddProduct extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+    }
+}
